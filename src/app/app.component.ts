257b0fa862e03a520e32965a234d639af24cebeb
hakankucuk <hakankck@gmail.com>
2023-10-09 01:23:15 +0300

    deleted-multiselect-boolean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DragDropModule,
-  CdkDragHandle,CdkDrag,CdkDropListGroup,CdkDragMove,
-  CdkDragStart,CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  DragDropModule,
+  CdkDragHandle, CdkDrag, CdkDropListGroup, CdkDragMove,
+  CdkDragStart, CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -11,87 +13,80 @@ import {DragDropModule,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  stored:any=[];
-  ctrlPress=false;
-  isMultidragging=false;
+  stored: any = [];
+  ctrlPress = false;
   todo = [
     {
-      name:'Get to work',
-      selected:false,
-      isMultidragging:false
+      name: 'Get to work',
+      selected: false,
     },
     {
-      name:'Pick up groceries',
-      selected:false
-    
-    },
-    {
-     name:'Go home',
-      selected:false
-     },
+      name: 'Pick up groceries',
+      selected: false
 
-    {name:'Fall asleep',
-    selected:false
-     }
+    },
+    {
+      name: 'Go home',
+      selected: false
+    },
+
+    {
+      name: 'Fall asleep',
+      selected: false
+    }
   ];
 
   done = [
     {
-      name:'Wash hair',
-      selected:false,
-      isMultidragging:false
+      name: 'Wash hair',
+      selected: false,
     },
     {
       name: 'Brush teeth',
-      selected:false
+      selected: false
     },
     {
-      name:'Go home',
-      selected:false
-     },
+      name: 'Go home',
+      selected: false
+    },
     {
-      name:'Check e-mail',
-      selected:false
-     },
+      name: 'Check e-mail',
+      selected: false
+    },
     {
       name: 'Walk dog',
-      selected:false
-     }
-    ];
+      selected: false
+    }
+  ];
 
   drop(event: CdkDragDrop<any[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      if(this.stored.length > 0){
-        event.previousContainer.data.slice(0).reverse().forEach(function(item,idx){
-           if (item.selected) {
+      if (this.stored.length > 0) {
+        event.previousContainer.data.slice(0).reverse().forEach(function (item, idx) {
+          if (item.selected) {
             event.previousContainer.data.splice(event.previousContainer.data.indexOf(item), 1);
-          
-            event.container.data.splice(event.currentIndex,0,item)
-            event.container.data.forEach(function(d){
-              d.isMultidragging=false;
-              d.selected=false;
+            event.container.data.splice(event.currentIndex, 0, item)
+            event.container.data.forEach(function (d) {
+              d.selected = false;
             })
-           
-           }
+
+          }
         });
-       this.stored=[];
 
-     }else{
-      
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-                        
-    }
-
+        this.stored = [];
+      } else {
+        transferArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
       }
+    }
 
   }
   // test(event: CdkDragStart<any[]>){
-       
+
   //   if(this.ctrlPress !==false && this.stored.length > 0){
   //        for(let item of event.source.dropContainer.data){
 
@@ -104,17 +99,16 @@ export class AppComponent {
   //   }
 
   // }
-    onKeyDown(e:MouseEvent ,item:any,data:any){
-      this.ctrlPress=e.ctrlKey;
+  onKeyDown(e: MouseEvent, item: any, data: any) {
+    this.ctrlPress = e.ctrlKey;
 
-     if (e.ctrlKey && this.stored.indexOf(item)==-1){
-        item.selected=true;
-        let idx=data.indexOf(item);
-        item.selected=true;
-        item.index=idx;
-       
-        this.stored.push(item)
-     } 
+    if (e.ctrlKey && this.stored.indexOf(item) == -1) {
+      item.selected = true;
+      let idx = data.indexOf(item);
+      item.selected = true;
+      item.index = idx;
+      this.stored.push(item)
+    }
 
   }
 }
