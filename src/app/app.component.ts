72ec729c46f185d927822621e9641affca88437c
hakankucuk <hakankck@gmail.com>
2023-10-08 23:42:27 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DragDropModule,
+  CdkDragHandle,CdkDrag,CdkDropListGroup,CdkDragMove,
+  CdkDragStart,CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  stored:any=[];
+  ctrlPress=false;
+  isMultidragging=false;
+  todo = [
+    {
+      name:'Get to work',
+      selected:false,
+      isMultidragging:false
+    },
+    {
+      name:'Pick up groceries',
+      selected:false
+    
+    },
+    {
+     name:'Go home',
+      selected:false
+     },
+
+    {name:'Fall asleep',
+    selected:false
+     }
+  ];
+
+  done = [
+    {
+      name:'Wash hair',
+      selected:false,
+      isMultidragging:false
+    },
+    {
+      name: 'Brush teeth',
+      selected:false
+    },
+    {
+      name:'Go home',
+      selected:false
+     },
+    {
+      name:'Check e-mail',
+      selected:false
+     },
+    {
+      name: 'Walk dog',
+      selected:false
+     }
+    ];
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      if(this.stored.length > 0){
+        event.previousContainer.data.slice(0).reverse().forEach(function(item,idx){
+           if (item.selected) {
+            event.previousContainer.data.splice(event.previousContainer.data.indexOf(item), 1);
+          
+            event.container.data.splice(event.currentIndex,0,item)
+            event.container.data.forEach(function(d){
+              d.isMultidragging=false;
+              d.selected=false;
+            })
+           
+           }
+        });
+       this.stored=[];
+
+     }else{
+      
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+                        
+    }
+
+      }
+
+  }
+  // test(event: CdkDragStart<any[]>){
+       
+  //   if(this.ctrlPress !==false && this.stored.length > 0){
+  //        for(let item of event.source.dropContainer.data){
+
+  //         if(item.selected){
+  //          item.isMultidragging=true;
+  //         }else {
+  //           item.isMultidragging=false;
+  //         }
+  //       }
+  //   }
+
+  // }
+    onKeyDown(e:MouseEvent ,item:any,data:any){
+      this.ctrlPress=e.ctrlKey;
+
+     if (e.ctrlKey && this.stored.indexOf(item)==-1){
+        item.selected=true;
+        let idx=data.indexOf(item);
+        item.selected=true;
+        item.index=idx;
+       
+        this.stored.push(item)
+     } 
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule,
+  CdkDragHandle,CdkDrag,CdkDropListGroup,CdkDragMove,
+  CdkDragStart,CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CommonModule, NgFor } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule, NgFor, DragDropModule, CdkDropListGroup, CdkDropList, CdkDrag
   ],
   providers: [],
   bootstrap: [AppComponent]
